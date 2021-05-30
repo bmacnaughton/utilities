508476926bfb60cb79360eb7b438beba749920b3
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2021-05-30 05:43:29 -0700

    add permutation generators

--- permutations.js
+++ permutations.js
@@ -16,21 +16,53 @@ function permutations (arr, n = arr.length, out = []) {
   return out;
 }
 
-function* genPermutations (arr, n = arr.length, out = []) {
+function* genPermutationsV0 (arr, n = arr.length, out = []) {
   for (let i = 0; i < n; i++) {
     // if down to one remaining just use it
     if (n === 2) {
       yield arr.slice();
-      //out.push(arr.slice());
     } else {
-      permutations(arr, n - 1, out);
+      yield* genPermutations(arr, n - 1);
     }
     const j = n % 2 == 0 ? i : 0;
     const t = arr[n - 1];
     arr[n - 1] = arr[j];
     arr[j] = t;
   }
-  yield out;
 }
 
-module.exports = {permutations, genPermutations};
+function* genPermutations (arr, n = arr.length) {
+  const factorials = [1];
+  for (let i = 1; i <= n; i++) {
+    factorials[i] = factorials[i - 1] * i;
+  }
+
+  for (let i = 0; i < factorials[n]; i++) {
+    const onePermutation = [];
+    const temp = arr.slice();
+    let positionCode = i;
+    for (let position = arr.length; position > 0; position--) {
+      const selected = Math.floor(positionCode / factorials[position - 1]);
+      onePermutation.push(temp[selected]);
+      positionCode = positionCode % factorials[position - 1];
+      temp.splice(selected, 1);
+    }
+    yield onePermutation;
+  }
+}
+
+module.exports = {permutations, genPermutations, genPermutationsV0};
+
+if (!module.parent) {
+  const gp = genPermutations;
+  const dOdd = [1, 2, 4];
+  const dEven = [1, 2, 4, 8];
+
+  let r = [...gp(dOdd)];
+  /* eslint-disable no-console */
+  console.log(r);
+  r = [...gp(dEven)];
+
+  console.log(r);
+
+}
