047d80fca8b100c989ed6c46c6ea4d2807b94343
Bruce A. MacNaughton <bruce.macnaughton@contrastsecurity.com>
2021-08-04 14:39:36 -0700

    add combinations

---
+++ combinations.js
@@ -0,0 +1,13 @@
+// Generate all combinations of array elements
+// https://stackoverflow.com/questions/15298912/javascript-generating-combinations-from-n-arrays-with-m-elements
+//
+function* combinations(head, ...tail) {
+  const remainder = tail.length ? combinations(...tail) : [[]];
+  for (const r of remainder) {
+    for (const h of head) {
+      yield [h, ...r];
+    }
+  }
+}
+
+module.exports = combinations;
