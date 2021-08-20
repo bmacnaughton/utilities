41c437d5baea3690bf7bd576996142213d448656
Bruce A. MacNaughton <bruce.macnaughton@contrastsecurity.com>
2021-08-20 05:40:39 -0700

    fix eslint issues

--- test/debounce.test.js
+++ test/debounce.test.js
@@ -1,16 +1,18 @@
+'use strict';
+
 const expect = require('chai').expect;
 
 const Debounce = require('../debounce');
 
-describe('test debounce wrapper', function () {
-  it('should debounce by count with default delta of 100', function () {
+describe('test debounce wrapper', function() {
+  it('should debounce by count with default delta of 100', function() {
     // how many times debouncedFunc was called.
     let count = 0;
     // lastCount's expected value each time debouncedFunc is called.
     let expectedCount = 1;
 
     let i;
-    function debouncedFunc () {
+    function debouncedFunc() {
       expect(this.lastCount).equal(i);
       expect(this.lastCount).equal(expectedCount);
       expectedCount += db.deltaC;
@@ -24,14 +26,14 @@ describe('test debounce wrapper', function () {
     expect(count).equal(10);
   });
 
-  it('should debounce by count with delta of 500', function () {
+  it('should debounce by count with delta of 500', function() {
     // how many times debouncedFunc was called.
     let count = 0;
     // lastCount's expected value each time debouncedFunc is called.
     let expectedCount = 1;
     let i;
 
-    function debouncedFunc () {
+    function debouncedFunc() {
       expect(this.lastCount).equal(i);
       expect(this.lastCount).equal(expectedCount);
       expectedCount += db.deltaC;
@@ -40,7 +42,7 @@ describe('test debounce wrapper', function () {
 
     const db = new Debounce(debouncedFunc, {deltaCount: 500});
 
-    count = 0
+    count = 0;
     expectedCount = 1;
     for (i = 1; i < 1001; i++) {
       db.execute();
@@ -48,7 +50,7 @@ describe('test debounce wrapper', function () {
     expect(count).equal(2);
   });
 
-  it('should debounce repetitive logging by time', function (done) {
+  it('should debounce repetitive logging by time', function(done) {
     this.timeout(5000);
     // don't have mocha highlight this (even yellow) as a slow test.
     this.slow(10000);
@@ -63,7 +65,7 @@ describe('test debounce wrapper', function () {
     let count = 0;
     let expectedCount = 0;
 
-    function debouncedFunc () {
+    function debouncedFunc() {
       count += 1;
     }
 
@@ -71,7 +73,7 @@ describe('test debounce wrapper', function () {
 
     let i = 0;
     // how many seconds to let the test run.
-    const id = setInterval(function () {
+    const id = setInterval(function() {
       i += 1;
       if (i >= kMaxSeconds) {
         clearInterval(id);
@@ -82,13 +84,13 @@ describe('test debounce wrapper', function () {
         expect(db.count).equal(expectedCount);
         done();
       }
-    }, 1000)
+    }, 1000);
 
     // invoke debounced function every 10 ms
-    const lid = setInterval(function () {
+    const lid = setInterval(function() {
       expectedCount += 1;
       db.execute();
     }, 10);
 
-  })
-})
+  });
+});
